7a867564e39228b8f494b11a96fdf7fb99be977b
Grigory K. Bukin <bukin@giftery.ru>
2024-12-28 13:14:04 +0400

    Seeder for local and production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        if (!app()->isProduction()) {
+        if (app()->isLocal()) {
             User::factory()->create([
                 'name' => 'dev',
                 'email' => 'dev@thecove.com',
                 'password' => bcrypt('dev'),
+            ]);
+        }
+
+        if (app()->isProduction()) {
+            User::factory()->create([
+                'name' => 'admin',
+                'email' => 'admin@thecove.com',
+                'password' => bcrypt('mY-p@$$w0Rd-$tR0nG'),
             ]);
         }
     }
